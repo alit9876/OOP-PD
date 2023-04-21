aa4cca71f555c1d6bb83999010caa9db9e69e2a9
dell <m.alit9876@gmail.com>
2023-04-21 17:14:20 +0500

    Week 03

---
+++ Week 03/Oop/Week-03/Week-03/BL/Stident.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_03.BL
+{
+    class Stident
+    {
+        public string sname;
+        public float matricmarks;
+        public float fscmarks;
+        public float ecatmark;
+        public float aggregate;
+    }
+    class Student
+    {
+        //public Student()
+        //{
+        //    Console.WriteLine("Default Constructor called");
+        //}
+        public Student()
+            {
+            sname = "Jill";
+            }
+
+        public string sname;
+        public float matricmarks;
+        public float fscmarks;
+        public float ecatmark;
+        public float aggregate;
+    }
+    class Student1
+    {
+        public Student1()
+        {
+            sname = "Ali";
+            matricmarks = 990F;
+            fscmarks = 1050F;
+            ecatmark = 315F;
+            aggregate = 3.77F;
+        }
+     
+        public string sname;
+        public float matricmarks;
+        public float fscmarks;
+        public float ecatmark;
+        public float aggregate;
+    }
+    class Student2
+    {
+        public Student2(string n)
+        {
+            sname = n;
+        }
+
+        public string sname;
+        public float matricmarks;
+        public float fscmarks;
+        public float ecatmark;
+        public float aggregate;
+    }
+    class Student3
+    {
+        public Student3(string n,float m, float f, float e, float ag)
+        {
+            sname = n;
+            matricmarks = m;
+            fscmarks = f;
+            ecatmark = e;
+            aggregate = ag;
+        }
+
+        public string sname;
+        public float matricmarks;
+        public float fscmarks;
+        public float ecatmark;
+        public float aggregate;
+    }
+    class student9
+    {
+        public string sname;
+        public float matricMarks;
+        public float fscMarks;
+        public float ecatMarks;
+        public float aggregate;
+
+        public student9()
+        {
+            Console.Write("Default Constructor Called ");
+        }
+        public student9(string sname)
+        {
+            this.sname = sname;
+        }
+        public student9(student9 s)
+        {
+            sname = s.sname;
+            matricMarks = s.matricMarks;
+            fscMarks = s.fscMarks;
+            ecatMarks = s.ecatMarks;
+            aggregate = s.aggregate;
+        }
+    }
+    class clockType
+    {
+        public int hours;
+        public int minutes;
+        public int seconds;
+
+        public clockType()
+        {
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+        public clockType(int hours)
+        {
+            this.hours = hours;
+        }
+        public clockType(int hours, int minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+        public clockType(int hours, int minutes, int seconds)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+        public void incrementSecond()
+        {
+            seconds++;
+        }
+        public void incrementHour()
+        {
+            hours++;
+        }
+        public void incrementMinute()
+        {
+            minutes++;
+        }
+        public void printTime()
+        {
+            Console.WriteLine(hours + " " + minutes + " " + seconds);
+        }
+        public bool isEqual(int h, int m, int s)
+        {
+            if (hours == h && minutes == m && seconds == s)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isEqual(clockType t)
+        {
+            if (hours == t.hours && minutes == t.minutes && seconds == t.seconds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+    class Product
+    {
+        public string name;
+        public string category;
+        public float price;
+        public int quantity;
+        public int minimum;
+
+        public Product()
+            {
+
+            }
+        public void view()
+        {
+                Console.WriteLine(" Product Name: " + name + "  Category: " + category + "  Price: " + price + "  Stock: " + quantity); 
+        }
+        public string highestprice( List<Product> s)
+        {
+            float highest = 0;
+            string name = "";
+            for(int x =0; x < s.Count; x++)
+            {
+                if(highest < s[x].price)
+                {
+                    highest = s[x].price;
+                    name = s[x].name;
+                }
+            }
+            return name; 
+        }
+        public string ordered()
+        {
+            string ordered = "";
+            if(minimum < 10)
+            {
+                ordered = name;
+            }
+            return ordered;
+        }
+        public float caltaxes()
+        {
+            float tax = 0F;
+            if (category == "Grocery")
+            {
+                tax = (price * 10) / 100;
+            }
+            if (category == "fruit")
+            {
+                tax = (price * 5) / 100;
+            }
+            if (category != "fruit" && category != "Grocery")
+            {
+                tax = (price * 15) / 100;
+            }
+            return tax;
+        }
+    }
+    class clock
+    {
+        public int hours;
+        public int minutes;
+        public int seconds;
+        public int Remhours;
+        public int Remminutes;
+        public int Remseconds;
+        public int difhours;
+        public int difminutes;
+        public int difseconds;
+
+        public clock(int hour, int minute, int second)
+        {
+            this.hours = hour;
+            this.minutes = minute;
+            this.seconds = second;
+            if (minutes != 0 && second != 0)
+            {
+                Remhours = 24 - hours - 1;
+            }
+            else if (minutes == 0 && second == 0)
+            {
+                Remhours = 24 - hours;
+            }
+            Remminutes = 60 - minutes - 1;
+            Remseconds = 60 - seconds;
+            if (Remhours < 0)
+            {
+                if (hours == 24)
+                {
+                    Remhours = 0;
+                }
+                Remminutes = Remminutes * -1;
+                Remseconds = Remseconds * -1;
+            }
+        }
+        public void dif(int dhour, int dminute, int dsecond)
+        {
+            this.difhours = dhour;
+            this.difminutes = dminute;
+            this.difseconds = dsecond;
+            difhours = hours - dhour;
+            difminutes = minutes - dminute;
+            difseconds = seconds - dsecond;
+            if (difhours < 0)
+            {
+                if (dhour == 24)
+                {
+                    difhours = 0;
+                }
+                difminutes = difminutes * -1;
+                difseconds = difseconds * -1;
+            }
+        }
+        public void printdClock(int hour, int minute, int second)
+        {
+            Console.WriteLine("Clock: " + hour + " " + minute + " " + second);
+        }
+        public void printRTime()
+        {
+            Console.WriteLine("Remaining: " + Remhours + " " + Remminutes + " " + Remseconds);
+        }
+        public void printdTime()
+        {
+            Console.WriteLine("Difference: " + difhours + " " + difminutes + " " + difseconds);
+        }
+        public void printTime()
+        {
+            Console.WriteLine("Elapsed: " + hours + " " + minutes + " " + seconds);
+        }
+
+    }
+}
---
+++ Week 03/Oop/Week-03/Week-03/Program.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_03.BL;
+using System.Threading;
+
+namespace Week_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //t1();
+            //t2();
+            //t3();
+            //self1();
+            //self2();
+            //t4();
+            //sel3();
+            //sel4();
+            //t5();
+            //t6();
+            //t6b();
+            //t7();
+            //t8();
+            //t9();
+        }
+
+        static void t1()
+        {
+            Stident s1 = new Stident();
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s1.matricmarks);
+            Console.WriteLine(s1.fscmarks);
+            Console.WriteLine(s1.ecatmark);
+            Console.WriteLine(s1.aggregate);
+            Console.ReadKey();
+        }
+        static void t2()
+        {
+            Student s1 = new Student();
+            Console.ReadLine();
+        }
+        static void t3()
+        {
+            Student s1 = new Student();
+            Console.WriteLine(s1.sname);
+            Console.Read();
+        }
+        static void self1()
+        {
+            Student1 s1 = new Student1();
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s1.matricmarks);
+            Console.WriteLine(s1.fscmarks);
+            Console.WriteLine(s1.ecatmark);
+            Console.WriteLine(s1.aggregate);
+            Console.ReadKey();
+        }
+        static void self2()
+        {
+            Student1 s1 = new Student1();
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s1.matricmarks);
+            Console.WriteLine(s1.fscmarks);
+            Console.WriteLine(s1.ecatmark);
+            Console.WriteLine(s1.aggregate);
+
+            Student1 s2 = new Student1();
+            Console.WriteLine(s2.sname);
+            Console.WriteLine(s2.matricmarks);
+            Console.WriteLine(s2.fscmarks);
+            Console.WriteLine(s2.ecatmark);
+            Console.WriteLine(s2.aggregate);
+            Console.ReadKey();
+        }
+        static void t4()
+        {
+            Student2 s1 = new Student2("John");
+            Console.WriteLine(s1.sname);
+
+            Student2 s2 = new Student2("Jill");
+            Console.WriteLine(s2.sname);
+            Console.ReadKey();
+        }
+        static void sel3()
+        {
+            Student3 s1 = new Student3("John", 90F,1100F,315F,3.77F);
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s1.matricmarks);
+            Console.WriteLine(s1.fscmarks);
+            Console.WriteLine(s1.ecatmark);
+            Console.WriteLine(s1.aggregate);
+            Console.ReadKey();
+        }
+        static void sel4()
+        {
+            Student3 s1 = new Student3("John", 90F, 1100F, 315F, 3.77F);
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s1.matricmarks);
+            Console.WriteLine(s1.fscmarks);
+            Console.WriteLine(s1.ecatmark);
+            Console.WriteLine(s1.aggregate);
+
+
+            Student3 s2 = new Student3("John", 90F, 1100F, 315F, 3.77F);
+            Console.WriteLine(s2.sname);
+            Console.WriteLine(s2.matricmarks);
+            Console.WriteLine(s2.fscmarks);
+            Console.WriteLine(s2.ecatmark);
+            Console.WriteLine(s2.aggregate);
+            Console.ReadKey();
+        }
+        static void t5()
+        {
+            student9 s1 = new student9();
+            Console.WriteLine();
+            s1.sname = "Ali";
+            student9 s2 = new student9(s1);
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s2.sname);
+            Console.ReadKey();
+        }
+        static void t6()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for(int i=0; i <numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();
+        }
+        static void t6b()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();
+        }
+        static void t7()
+        {
+
+            //              Default Constructor
+
+            clockType emptyTime = new clockType();
+            Console.Write("Empty Time");
+            emptyTime.printTime();
+
+            //              Parametric Constructor (Hour)
+
+            clockType hour = new clockType(8);
+            Console.Write("Hour TIme: ");
+            hour.printTime();
+
+            //              Parametric Constructor (Hour, Minute)
+
+            clockType min = new clockType(8, 10);
+            Console.Write("Minute TIme: ");
+            min.printTime();
+
+            //              Parametric Constructor (Hour, Minute, Second)
+
+            clockType fullTime = new clockType(8, 10, 10);
+            Console.Write("Full TIme: ");
+            fullTime.printTime();
+
+            //             Icrement Hours
+
+            fullTime.incrementHour();
+            Console.Write("Full Time (Increment Hours): ");
+            fullTime.printTime();
+
+            //             Icrement Seconds
+
+            fullTime.incrementSecond();
+            Console.Write("Full Time (Increment Seconds): ");
+            fullTime.printTime();
+
+            //             Icrement Seconds
+
+            fullTime.incrementMinute();
+            Console.Write("Full Time (Increment Minutes): ");
+            fullTime.printTime();
+
+            //             Check if Equal 
+
+            bool flag = fullTime.isEqual(9, 11, 11);
+            Console.WriteLine("Flag: " + flag);
+
+            //             Check if Equal 
+
+            clockType cmp = new clockType(10, 12, 1);
+            flag = fullTime.isEqual(cmp);
+            Console.WriteLine("Object Flag: " + flag);
+
+            Console.ReadKey();
+        }
+        static void t9()
+        {
+            int choice = 8;
+
+            List<Product> s1 = new List<Product>();
+
+            while (choice != 0)
+            {
+
+                choice = menu();
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    Product s = new Product();
+                    Console.Write("ENter name of Product: ");
+                    s.name = Console.ReadLine();
+                    Console.Write("ENter category: ");
+                    s.category = Console.ReadLine();
+                    Console.Write("ENter Price: ");
+                    s.price = float.Parse(Console.ReadLine());
+                    Console.Write("ENter Quantity: ");
+                    s.quantity = int.Parse(Console.ReadLine());
+                    Console.Write("ENter Minmum: ");
+                    s.minimum = int.Parse(Console.ReadLine());
+                    s1.Add(s);
+
+                }
+                if (choice == 2)
+                {
+                    Console.Clear();
+                    for(int x=0; x < s1.Count; x++)
+                    {
+                        s1[x].view();
+                    }
+                    Console.ReadKey();
+                }
+                if (choice == 3)
+                {
+                    Console.Clear();
+                    Product s = new Product();
+                    string highest = s.highestprice(s1);
+                    Console.WriteLine("Product with Highest Unit Price: " + highest);
+                    Console.ReadKey();
+                }
+                if (choice == 4)
+                {
+                    Console.Clear();
+                    for (int x = 0; x < s1.Count; x++)
+                    {
+                        float tax = s1[x].caltaxes();
+                        Console.WriteLine("  Product Name:  " + s1[x].name + "  Tax Price is:  " + tax);
+                    }
+
+                    Console.ReadKey();
+                }
+                if (choice == 5)
+                {
+                    Console.Clear();
+                    for (int x = 0; x < s1.Count; x++)
+                    {
+                        string ordered = s1[x].ordered();
+                        if (ordered != "")
+                        {
+                            Console.WriteLine("The product to be ordered are: " + ordered);
+                        }
+
+                    }
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            }
+        }
+        static int menu()
+        {
+            Console.Clear();
+            Console.WriteLine(" 1. Add Product ");
+            Console.WriteLine(" 2. View All Product ");
+            Console.WriteLine(" 3. Find Product with Highest Unit Price");
+            Console.WriteLine(" 4. View sales tax of all Product ");
+            Console.WriteLine(" 5. Products to be Orderd ");
+            Console.WriteLine(" 0. Exit");
+            int choice;
+            choice = int.Parse(Console.ReadLine());
+            return choice;
+        }
+        static void t8()
+        {
+
+            Console.Clear();
+            clock s1 = new clock(22, 10, 10);
+            s1.dif(22, 8, 8);
+            s1.printTime();
+            s1.printRTime();
+            s1.printdClock(22, 8, 8);
+            s1.printdTime();
+            Console.ReadKey();
+        }
+
+    }
+}
